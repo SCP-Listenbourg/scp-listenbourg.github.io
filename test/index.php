865bc81ac8643d45f2f89d44f771bf42f117ad73
SCP Listenbourg <117569687+SCP-Listenbourg@users.noreply.github.com>
2022-11-20 22:03:06 +0100

    Create index.php

---
+++ test/index.php
@@ -0,0 +1,52 @@
+<DOCTYPE html>
+<html>
+<title>SCP-002-LS - SCP Listenbourg</title>
+<meta name="viewport" content="initial-scale=0.4">
+<link rel="shortcut icon" href="https://raw.githubusercontent.com/SCP-Listenbourg/scp-listenbourg.github.io/main/favicon.ico"/>
+<link rel="apple-touch-icon" href="https://raw.githubusercontent.com/SCP-Listenbourg/scp-listenbourg.github.io/main/favicon.ico"/>
+<link rel="icon" sizes="32x32" href="https://raw.githubusercontent.com/SCP-Listenbourg/scp-listenbourg.github.io/main/favicon.ico"/>
+<link rel="icon" sizes="16x16" href="https://raw.githubusercontent.com/SCP-Listenbourg/scp-listenbourg.github.io/main/favicon.ico"/>
+<font face="arial,helvetica">
+<body bgcolor="#FFFFFF">
+<h2>
+<center>
+<div style="padding-top:10px; padding-bottom:10px;border: 3px solid #000000; text-align: center;background: #000000;">
+<p><img src="https://raw.githubusercontent.com/SCP-Listenbourg/scp-listenbourg.github.io/main/logo_fondation_scp.png" height="150"></p>
+<p style="color:#FFFFFF";>FONDATION SCP</p>
+<p style="color:#FFFFFF";>LISTENBOURG</p>
+</div>
+</center>
+</h2>
+<p>&nbsp;</p>
+<center>
+<p><img src="https://raw.githubusercontent.com/SCP-Listenbourg/scp-listenbourg.github.io/main/SCP/002-LS/SCP-002-LS.jpg" height="250"></p>
+<p style="color:#620000";>Intérieure de SCP-002-LS, photographie prise le ██/██/1938, à ███████, Listenbourg.</p>
+<p>&nbsp;</p>
+</center>
+<strong><p style="color:#000000";>Objet :</strong> SCP-002-LS</p>
+<strong><p style="color:#000000";>Niveau de Menace :</strong> Jaune</p>
+<strong><p style="color:#000000";>Classe :</strong> Euclide</p>
+<strong><p style="color:#000000";>Procédures de Confinement Spéciales :</strong> la porte d'accès en métal de SCP-002-LS doit impérativement restée fermée, il est formellement interdit de pénétrer dans la salle, ou d'y insérer un quelconque objet. Seuls les cobayes volontaires pour expérience sont autorisés à y entrer, en mettant en place une surveillance accrue par le biais de capteurs divers et de plusieurs caméras placées chacunes dans un coin de SCP-002-LS. Le temps maximal d'une expérience dans SCP-002-LS est limité à 5 heures.</p>
+<strong><p style="color:#000000";>Description :</strong> SCP-002-LS est une salle, d'un grand hangar abandonné dans le quartier nord de ███████, au Listenbourg. SCP-002-LS comporte une rangé de 4 piliers porteurs. En son milieu la salle comporte un trou de matière invisible à l'œil nu, une sphère de vide, de 75 centimètres de diamètre et qui réagit à la présence de matière organique dans son périmètre. Toutes insertions de matière organique à l'intérieur de SCP-002-LS entraîne une modification atomique ainsi qu'une déformation physique de cette dernière, altérant la réalité. Les causes de ce phénomène sont encore inexplicable.</p>
+<h1><p style="color:#620000";>RAPPORTS D'EXPÉRIENCES :</p></h1>
+<em><p style="color:#000000";>Durant ces expériences, chaque objet a été transporté et déposé au milieu de SCP-002-LS, sous la sphère de vide, par le robot mobile numéro █. Toutes les expériences ont été dirigées et surveillées par le Docteur █████, en appliquant les mesures de sécurité données.</p></em>
+<strong><p style="color:#000000";>- Expérience numéro 1 :</strong> Cube en plomb, le ██/██/1938.</p>
+Précisions : l'objet fait 20 centimètres cubes. Résultats de l'expérience : le cube en plomb est resté intacte.
+<strong><p style="color:#000000";>- Expérience numéro 2 :</strong> Coquelicot, le ██/██/1938.</p>
+Précisions : le coquelicot mesure 25 centimètres de haut, il est planté dans un pot contenant de la terre. Résultats de l'expérience : les pétales sont passés du rouge au bleu foncé, sa taille à augmenté à 32 centimètres, mais le coquelicot est mort.
+<strong><p style="color:#000000";>- Expérience numéro 3 :</strong> Rat, le ██/██/1941.</p>
+Précisions : le rat mesure 22 centimètres, sans compter la queue. Résultats de l'expérience : le rat semble être malade, il est immobile mais reste en vie, une deuxième queue a poussé à côté de la première.
+<strong><p style="color:#000000";>- Expérience numéro 4 :</strong> Humain, le ██/██/████.</p>
+<p style="color:#000000";>[RÉSULTATS BLOQUÉS]</p>
+<p>&nbsp;</p>
+<center>
+<p><img src="https://raw.githubusercontent.com/SCP-Listenbourg/scp-listenbourg.github.io/main/SCP/002-LS/SCP-002-LS_sphere.png" height="250"></p>
+<p style="color:#620000";>Représentation de la déformation de l'espace créée par SCP-002-LS en son milieu.</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><a href="https://scp-listenbourg.github.io/accueil/" style="color:#000000";>ACCUEIL</a></p>
+<p>&nbsp;</p>
+</center>
+</body>
+</font>
+</html> 
