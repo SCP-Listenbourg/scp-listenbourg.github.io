9c099880e8299c4fc1735225848daed9ad76aab2
SCP Listenbourg <117569687+SCP-Listenbourg@users.noreply.github.com>
2022-11-20 22:12:30 +0100

    Create index.php

---
+++ test/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Comment mettre PHP en HTML - Exemple simple</title>
+</head>
+<body>
+<h1><?php echo "Ce message provient du côté serveur."; ?></h1>
+</body>
+</html>
